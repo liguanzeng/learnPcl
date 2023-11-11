2b098c0fb3c0000d1f421f5b78a579bbb137df55
liguanzeng <1245842028@qq.com>
2023-11-11 22:51:54 +0800

    add openni_grabber code

--- 00_base/04_openni_grabber/openni_grabber.cpp
+++ 00_base/04_openni_grabber/openni_grabber.cpp
@@ -11,4 +11,36 @@ class SimpleOpenNIViewer
     public:
         SimpleOpenNIViewer (): viewer("PCL OpenNI Viewer!"){}
 
+        void cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
+        {
+            if (!viewer.wasStopped())
+                viewer.showCloud(cloud);
+        }
+
+        void run()
+        {
+            pcl::Grabber* interface = new pcl::OpenNIGrabber();
+
+            std::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f = 
+                [this](const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud){cloud_cb_(cloud);};
+
+            interface->registerCallback(f);
+            interface->start();
+
+            while (!viewer.wasStopped())
+            {
+                std::this_thread::sleep_for(1s);
+            }
+            
+            interface->stop();
+        }
+
+        pcl::visualization::CloudViewer viewer;
+};
+
+int main()
+{
+    SimpleOpenNIViewer v;
+    v.run();
+    return 0;
 }
