f2f16f2e7c6dac2d3f55c3d515cbe337c6614b74
liguanzeng <1245842028@qq.com>
2023-11-26 12:15:15 +0800

    add comments

--- 01_filter/02_PassThrough_filter/passthrough.cpp
+++ 01_filter/02_PassThrough_filter/passthrough.cpp
@@ -2,6 +2,7 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 // 定义点云格式
 typedef pcl::PointXYZ PointT;
@@ -30,10 +31,11 @@ int main() {
 
     //Create the filter obj
     pcl::PassThrough<PointT> pass;
-    pass.setInputCloud(cloud);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(0.0, 1.0);
-    pass.filter(*cloud_filtered);
+    pass.setInputCloud(cloud);              // 1.设置输入源
+    pass.setFilterFieldName("z");           // 2.设置过滤域名
+    pass.setFilterLimits(0.0, 1.0);         // 3.设置过滤范围
+    // pass.setFilterLimitsNegative(true);     // 设置获取Limits之外的内容,结果取反
+    pass.filter(*cloud_filtered);           // 4.执行filter,并将结果保存至cloud_filtered
 
     std::cerr << "Clouds after filtering: " << std::endl;
     for (const auto& point : *cloud_filtered) {
@@ -41,6 +43,14 @@ int main() {
                               << point.y << " "
                               << point.z << " " << std::endl;
     }
+
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+
+    // 这里会一直阻塞直到点云被渲染
+    viewer.showCloud(cloud);
+    while (!viewer.wasStopped()){
+    }
+    
 
     return 0;
 }
